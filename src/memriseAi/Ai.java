419a8e5edb8e9e455464d7c7251f3ac0bc9fa090
Ryan Stouffer2 <ryry2nd@gmail.com>
2023-01-18 09:03:37 -0500

    better findans

--- src/memriseAi/Ai.java
+++ src/memriseAi/Ai.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import exceptions.UnPassNotFoundException;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.WebDriver;
 
@@ -39,6 +38,7 @@ public class Ai {
         this.SADL = SADL;
         console = new Console(link);
     }
+    
 
     public void quit() {
         console.escape = true;
@@ -48,23 +48,13 @@ public class Ai {
 
     public static HashMap<String, String> getWords() {return words;}
 
-    
-    private void pressEnter() {
-        driver.findElement(By.xpath("//html")).sendKeys(Keys.ENTER);
-    }
-    private String getQuestion() {
-        return driver.findElement(By.xpath("//h2[@class='sc-af59h9-2 hDpNkj']")).getAccessibleName();
-    }
-    private void skipReminder(){
-        driver.findElement(By.xpath("//button[@class='sc-1dxc4vq-2 fjYiwU']")).click();
-    }
 
     private void typeInBox() {
-        driver.findElement(By.xpath("//input[@class='sc-1v1crxt-4 kHCLct']")).sendKeys(BasicFunctions.findAns(words, getQuestion()));
-        pressEnter();
+        driver.findElement(By.xpath("//input[@class='sc-1v1crxt-4 kHCLct']")).sendKeys(BasicFunctions.findAns(words, BasicFunctions.getQuestion(driver)));
+        BasicFunctions.pressEnter(driver);
     }
     private void multipleChoice() {
-        final String CORRECTANSWER = BasicFunctions.findAns(words, getQuestion());
+        final String CORRECTANSWER = BasicFunctions.findAns(words, BasicFunctions.getQuestion(driver));
         
         if (!CORRECTANSWER.equals("")) {
             List<WebElement> answers = driver.findElements(By.xpath("//button[@class='sc-bcXHqe iDigtw']"));;
@@ -88,7 +78,7 @@ public class Ai {
             }
         }
 
-        pressEnter();
+        BasicFunctions.pressEnter(driver);
     }
     private void learn() {
         final String QUESTION = driver.findElement(By.xpath("//h3[@class='sc-18hl9gu-6 hjLhBn']")).getAccessibleName();
@@ -96,8 +86,9 @@ public class Ai {
         
         if (!words.containsKey(QUESTION)) {words.put(QUESTION, ANS);}
         
-        pressEnter();
+        BasicFunctions.pressEnter(driver);
     }
+
 
     public void start() throws UnPassNotFoundException, InterruptedException {
         console.start();
@@ -112,19 +103,19 @@ public class Ai {
                         typeInBox();}
 
                     else if (!SADL && !driver.findElements(By.xpath("//div[@data-testid='course-leaderboard']")).isEmpty()) {
-                        if (((System.currentTimeMillis() - clock) > (5 * 60000)) || console.refresh) {driver.get(console.getLink()); clock = System.currentTimeMillis(); console.refresh = false;} else {pressEnter();}}
+                        if (((System.currentTimeMillis() - clock) > (5 * 60000)) || console.refresh) {driver.get(console.getLink()); clock = System.currentTimeMillis(); console.refresh = false;} else {BasicFunctions.pressEnter(driver);}}
 
                     else if (!driver.findElements(By.xpath("//button[@class='sc-bcXHqe iDigtw']")).isEmpty()) {
                         multipleChoice();}
                     
                     else if (!driver.findElements(By.xpath("//button[@class='sc-1umog8t-0 kFaJKr']")).isEmpty()) {
-                        pressEnter();}
+                        BasicFunctions.pressEnter(driver);}
                     
                     else if (!driver.findElements(By.xpath("//button[@class='sc-1dxc4vq-2 fjYiwU']")).isEmpty()) {
-                        skipReminder();}
+                        BasicFunctions.skipReminder(driver);}
 
                     else if (!driver.findElements(By.xpath("//a[@aria-label='Learn new words']")).isEmpty()) {
-                        pressEnter();}
+                        BasicFunctions.pressEnter(driver);}
 
                     else if (!driver.findElements(By.xpath("//div[@class='sc-s6iyrn-2 hldCEU']")).isEmpty()) {
                         driver.get(console.getLink());}
--- src/memriseAi/BasicFunctions.java
+++ src/memriseAi/BasicFunctions.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,15 +14,10 @@ import exceptions.UnPassNotFoundException;
 
 
 public class BasicFunctions {
-    public static String findAns(HashMap<String, String> words, String question) {//fixMe later
-        String listQuestion, listAns;
-        for (HashMap.Entry<String, String> entry : words.entrySet()) {
-            listQuestion = entry.getKey();
-            listAns = entry.getValue();
-            if (listQuestion.equals(question)) {return listAns;}
-            if (listAns.equals(question)) {return listQuestion;}
-        }
-        return "";
+    public static String findAns(HashMap<String, String> words, String question) {
+        String ans = words.get(question);
+        if (ans == null) {return "";}
+        return ans;
     }
 
     public static void log_in(WebDriver driver, String username, String password, String link) throws UnPassNotFoundException, InterruptedException{
@@ -50,5 +46,15 @@ public class BasicFunctions {
         un.sendKeys(username);
         passwd.sendKeys(password);
         submit.click();
+    }
+
+    public static void pressEnter(WebDriver driver) {
+        driver.findElement(By.xpath("//html")).sendKeys(Keys.ENTER);
+    }
+    public static String getQuestion(WebDriver driver) {
+        return driver.findElement(By.xpath("//h2[@class='sc-af59h9-2 hDpNkj']")).getAccessibleName();
+    }
+    public static void skipReminder(WebDriver driver) {
+        driver.findElement(By.xpath("//button[@class='sc-1dxc4vq-2 fjYiwU']")).click();
     }
 }
