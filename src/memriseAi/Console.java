826e73d53f71ee4bb92a039e4d34ed754d2662ad
Ryan Stouffer2 <ryry2nd@gmail.com>
2023-01-21 17:56:39 -0500

    complaint

--- src/memriseAi/Console.java
+++ src/memriseAi/Console.java
@@ -11,7 +11,8 @@ public class Console extends Thread {
     private Scanner sc;
     private String link;
 
-    private String help = "help: helps you\nexit: exits\nchlink: changes link\nrefresh: refreshes";
+    private final String help = "help: helps you\nexit: exits\nchlink: changes link\nrefresh: refreshes";
+    private final String complaint = "use /help you moron just like literally every other command line in existence. I mean seriously even minecraft has a /help, it's even in the name /help.";
 
     public Console(String link) {
         sc = new Scanner(System.in);
@@ -42,7 +43,7 @@ public class Console extends Thread {
             else if (command.equals("chlink")) {link = args[0]; refresh = true;}
             else if (command.equals("refresh")) {refresh = true;}
             else if (command.equals("cactuses")) {System.out.println("Unknown command \"\033[1mCACTI\033[0m\" use /help you moron just like literally every other command line in existence");}
-            else {System.out.println("Unknown command \"" + command + "\" use /help you moron just like literally every other command line in existence");}
+            else {System.out.println("Unknown command \"" + command + "\" " + complaint);}
         }
     }
 }
